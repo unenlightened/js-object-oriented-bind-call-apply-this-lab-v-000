576ea861fb3a27517b65c7036240fb423ec28d1c
Chely Ho <adept_inept@yahoo.com>
2018-07-04 20:21:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ function setThisWithApply(fn, obj, args) {
   return fn.apply(obj, args)
 }
 
+function returnNewFunctionOf(fn, obj) {
+  
+}
+
+describe('returnNewFunctionOf', function(){
+  let fred;
+  let functionToBeCopied;
+
+  beforeEach(function(){
+    functionToBeCopied = function (){
+      return this
+    }
+   fred = { name: 'fred'}
+  })
+  it('returns a new function', function(){
+    expect(returnNewFunctionOf(functionToBeCopied)).to.not.equal(functionToBeCopied)
+    expect(typeof returnNewFunctionOf(functionToBeCopied)).to.equal("function")
+  })
+  it('sets the this argument to fred', function(){
+    let newFunction = returnNewFunctionOf(functionToBeCopied, fred)
+    expect(newFunction()).to.equal(fred)
+  })
